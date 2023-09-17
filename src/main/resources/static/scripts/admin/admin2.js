61631acae23c5d4fdd4b350c78364317711db3be
SonyaBeldy <sofya.beldy@mail.ru>
2023-09-17 22:42:43 +1000

    js code refactor, some bug fix

--- src/main/resources/static/scripts/admin/admin2.js
+++ src/main/resources/static/scripts/admin/admin2.js
@@ -1,12 +1,17 @@
-if(sessionStorage.getItem('data')) {
-    document.getElementById(sessionStorage.getItem('data')).checked = true;
-} else {
-    document.getElementById("radio-dance-lists").checked = true;
-    sessionStorage.setItem("data", "radio-dance-lists");
-}
-
-
+setCheckedTab();
 showCheckedTable();
+let rows = new Map();
+let currentRow = new TableRow();
+function setCheckedTab() {
+    if(sessionStorage.getItem('data')) {
+        document.getElementById(sessionStorage.getItem('data')).checked = true;
+    } else {
+        document.getElementById("radio-dance-lists").checked = true;
+        sessionStorage.setItem("data", "radio-dance-lists");
+    }
+
+
+}
 async function showCheckedTable() {
     if(document.getElementById("radio-dance-lists").checked) {
         await fillTable(await getItemList("dance-lists"));
@@ -17,73 +22,155 @@ async function showCheckedTable() {
 async function getItemList(url) {
     return await fetch("http://localhost:8080/admin/" + url, {method: "GET"});
 }
+class EditMenu {
+    _id;
+    _menu;
+    constructor(menu) {
+        this._menu = menu;
+    }
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get menu() {
+        return this._menu;
+    }
+
+    set menu(value) {
+        this._menu = value;
+    }
+}
+let editMenu = new EditMenu(document.getElementById('edit-menu'));
 async function fillTable(listValues) {
     let listArray= (await listValues.json());
     for (let i = 0; i < listArray.length; i++) {
         let id = listArray[i].id;
         let name = listArray[i].name;
         let date = listArray[i].date;
+        let time = listArray[i].time;
+        let desc = listArray[i].description;
+
+        let tableRow = new TableRow(id, name, date, time, desc);
 
         const dateOptions = {
             day: '2-digit',
             month: 'long',
             year: 'numeric'
-            // hour: '2-digit',
-            // minute: '2-digit',
         };
 
-        // let newDate = new Date(date).toLocaleDateString('ru-RU', options);
+        const timeOptions = {
+            hour: '2-digit',
+            minute: '2-digit'
+        }
 
-        let newDate = new Date(date).toLocaleString('ru-RU', dateOptions);
-        let localeTime = new Date(date).toLocaleTimeString();
-        // d = d.replace(',', '');
-        let time = listArray[i].time;
-        // // d = d.replace(/[а-я]/, c => c.toUpperCase());
-        newDate = newDate.replace(' г.', '');
-
-        let desc = listArray[i].description;
+        // // let newDate = new Date(date).toLocaleDateString('ru-RU', options);
+        // let newDate = new Date(date).toLocaleString('ru-RU', dateOptions);
+        // let localeTime = new Date(date).toLocaleTimeString();
+        //
+        // newDate = newDate.replace(' г.', '');
         let listContainer = document.getElementById("table-items");
+        rows.set(tableRow.id, tableRow);
+        tableRow.name.addEventListener('dblclick', nameDoubleClick);
+        tableRow.date.addEventListener('dblclick', dateDoubleClick);
+        listContainer.appendChild(tableRow.row);
 
-        listContainer.appendChild(createTableItem(id, name, newDate, time, desc));
+
     }
+    document.getElementById('cancel').addEventListener("click", ev1 => {
+        editMenu.menu.style.display = "none";
+    })
+    document.getElementById('submit').addEventListener('click', async ev1 => {
+        let newName = document.getElementById("textarea").value;
+        let curRow = new TableRow();
+
+        curRow = rows.get(editMenu.id);
+        let obj = {id: editMenu.id, name: newName, date: parseCustomLocalizedDateTime(curRow.date.textContent), desc: 'desc'};
+        patch(obj, "dance-list/update");
+        editMenu.menu.style.display = "none";
+        curRow.name.textContent = newName;
+    })
+    window.addEventListener("resize", ev => {
+        if(editMenu.id != null) {
+            updateMenuPosition(document.getElementById("edit-menu"),  document.getElementById(editMenu.id));
+        }
+    });
+
 }
 
-function createTableItem(id, name, date, time, desc) {
+function nameDoubleClick(event) {
+    currentRow = event.target.parentNode;
 
-    let item = document.createElement("tr");
-    item.setAttribute("id", id);
-    item.addEventListener("click", ev => {
-        let nodes = document.getElementById("table").getElementsByTagName("tr");
-        for (let i = 0; i < nodes.length; i++) {
-            nodes[i].style.background = "#ffffff";
-        }
-        item.style.background = "#ecf0fd";
-    });
+    let menu = document.getElementById("edit-menu");
+    menu.style.display = "flex";
+
+    let nameElement = event.target;
+    editMenu.id = Number(nameElement.parentNode.id);
+    // currentRow = event.target.parentNode.id;
+    updateMenuPosition(editMenu.menu, nameElement);
+    document.getElementById("textarea").value = nameElement.textContent;
+}
+
+function dateDoubleClick(event) {
+    let ob = document.getElementById("input");
+    let currentDateNode = event.target;
+    xCal(currentDateNode,  {lang: 'ru', fn: (date, ob) => {
+            let parseDate = parseCalendarDateToDate(date);
+            let obj = {id: currentDateNode.parentNode.id, name: name, date: parseCustomLocalizedDateTime(parseDate), desc: 'desc'};
+            patch(obj, "dance-list/update");
+            currentDateNode.textContent = parseDate;
+        }  });
+}
+function setBackgroundToAll(nodes, color) {
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].style.background = color;
+    }
+}
+function highlightTableRow(row) {
+    let nodes = document.getElementById("table").getElementsByTagName("tr");
+    setBackgroundToAll(nodes, "#ffffff");
+    row.style.background = "#ecf0fd";
+}
+function createTableRow(id, name, date, time, desc) {
+
+    let row = document.createElement("tr");
+
+    let tableRow = new TableRow(id, name, date, time, desc);
+
+    row.setAttribute("id", id);
+    row.addEventListener("click", ev => highlightTableRow(row));
 
     let itemName = document.createElement("td");
     itemName.textContent = name;
-    itemName.addEventListener("dblclick", ev => {
-        let menu = document.getElementById("edit-menu");
-        menu.style.display = "flex";
-        updateMenuPosition(menu, itemName);
-        document.getElementById("textarea").value = name;
-
-        document.getElementById("cancel").addEventListener("click", ev1 => {
-            menu.style.display = "none";
-        })
-
-        document.getElementById("submit").addEventListener("click", async ev1 => {
-
-            let obj = {id: id, name: document.getElementById("textarea").value, date: parseCustomLocalizedDateTime(date), desc: desc};
-            updateData(obj, "dance-list/update");
-            menu.style.display = "none";
-            location.reload();
-        })
-
-        window.addEventListener("resize", ev => {
-            updateMenuPosition(document.getElementById("edit-menu"), itemName);
-        })
-    });
+    // itemName.addEventListener("dblclick", ev => {
+    //     let menu = document.getElementById("edit-menu");
+    //     menu.style.display = "flex";
+    //     updateMenuPosition(menu, itemName);
+    //
+    //     document.getElementById("textarea").value = name;
+    //
+    //     document.getElementById("cancel").addEventListener("click", ev1 => {
+    //         menu.style.display = "none";
+    //     })
+    //
+    //     let clone = document.getElementById('submit').cloneNode(true);
+    //     document.getElementById('submit').parentNode.replaceChild(clone, document.getElementById('submit'));
+    //     clone.addEventListener("click", async ev1 => {
+    //
+    //         let newName = document.getElementById("textarea").value;
+    //         let obj = {id: id, name: newName, date: parseCustomLocalizedDateTime(date), desc: desc};
+    //         patch(obj, "dance-list/update");
+    //         menu.style.display = "none";
+    //         itemName.textContent = newName;
+    //     })
+    //
+    //     window.addEventListener("resize", ev => {
+    //         updateMenuPosition(document.getElementById("edit-menu"), itemName);
+    //     })
+    // });
 
 
 
@@ -93,8 +180,8 @@ function createTableItem(id, name, date, time, desc) {
         let ob = document.getElementById("input");
         xCal(itemDate,  {lang: 'ru', fn: (date, ob) => {
                 let parseDate = parseCalendarDateToDate(date);
-                let obj = {id: id, name: document.getElementById("textarea").value, date: parseCustomLocalizedDateTime(parseDate), desc: desc};
-                updateData(obj, "dance-list/update");
+                let obj = {id: id, name: name, date: parseCustomLocalizedDateTime(parseDate), desc: desc};
+                patch(obj, "dance-list/update");
 
                 itemDate.textContent = parseDate;
             }  });
@@ -104,27 +191,27 @@ function createTableItem(id, name, date, time, desc) {
     itemTime.textContent = time;
 
     let itemDescription = document.createElement("td");
-    itemDescription.textContent = desc;
+    itemDescription.textContent = id;
     itemDescription.addEventListener("dblclick", ev => {
     })
 
     let itemState = document.createElement("td");
     itemState.textContent = "Нет";
 
-    item.appendChild(itemName);
-    item.appendChild(itemDate);
-    item.appendChild(itemTime);
-    item.appendChild(itemDescription);
-    item.appendChild(itemState);
+    row.appendChild(itemName);
+    row.appendChild(itemDate);
+    row.appendChild(itemTime);
+    row.appendChild(itemDescription);
+    row.appendChild(itemState);
 
-    return item;
+    return row;
 }
 function updateMenuPosition(menu, refElement) {
     let rec = refElement.getBoundingClientRect();
     menu.style.left = rec.left + "px";
     menu.style.top = (rec.top) + "px";
 }
-async function updateData(obj, url) {
+async function patch(obj, url) {
     await fetch("http://localhost:8080/" + url,
         {
             method: "PATCH",
---
+++ src/main/resources/static/scripts/admin/table.js
@@ -0,0 +1,178 @@
+class DanceListTable {
+    _rows; //list of table rows with html elements
+    table; //html table
+    constructor(table) {
+        this.table = table;
+    }
+    addRow(row) {
+        this.table.appendChild(row);
+    }
+
+    getRow(id) {
+        return this._rows[id];
+    }
+}
+
+class tableDAO {
+
+    async get(url) {
+        await fetch(url, {
+            method: 'GET'
+        })
+    }
+    async patch(url, obj) {
+        await fetch(url, {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(obj)
+        })
+    }
+
+    async post(url, obj) {
+        await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(obj)
+        })
+    }
+
+    async delete(url, obj) {
+        await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(obj)
+        })
+    }
+}
+
+function createElement(textContent) {
+    let element = document.createElement('td');
+    element.textContent = textContent;
+    return element;
+}
+
+class TableRow {
+    _row;
+    constructor(id, name, date, time, desc) {
+        this._row = document.createElement("tr");
+        this._row.setAttribute('id', id);
+        this._id = id;
+        this._name = createElement(name);
+        this._date = createElement(this.parseDate(date));
+        this._time = createElement(time);
+        this._desc = createElement(desc);
+        // this._status = createElement(status);
+
+        this._row.appendChild(this._name);
+        this._row.appendChild(this._date);
+        this._row.appendChild(this._time);
+        this._row.appendChild(this._desc);
+        // this._row.appendChild(this._status);
+    }
+
+    parseDate(date) {
+        const dateOptions = {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        };
+        let newDate = new Date(date).toLocaleString('ru-RU', dateOptions);
+        newDate = newDate.replace(' г.', '');
+        return newDate;
+    }
+
+    parseTime(time) {
+
+    }
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get row() {
+        return this._row;
+    }
+
+    set row(value) {
+        this._row = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    set date(value) {
+        this._date = value;
+    }
+
+    get time() {
+        return this._time;
+    }
+
+    set time(value) {
+        this._time = value;
+    }
+
+    get desc() {
+        return this._desc;
+    }
+
+    set desc(value) {
+        this._desc = value;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    set status(value) {
+        this._status = value;
+    }
+}
+
+class tableColumn {
+    doubleClickAction() {
+
+    }
+}
+class nameColumn extends tableColumn {
+    _clickBehavior;
+
+    get clickBehavior() {
+        return this._clickBehavior;
+    }
+
+    set clickBehavior(value) {
+        this._clickBehavior = value;
+    }
+}
+
+class dateColumn extends tableColumn {
+    doubleClickAction() {
+
+    }
+}
+
+
+class timeColumn extends tableColumn {
+    doubleClickAction() {
+        console.log("time click")
+    }
+}
